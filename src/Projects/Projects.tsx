6035ad320ca610896c3a0d74c95aa7d1e0c2263f
Santiago S <santiagoagredo@live.com>
2023-04-10 15:37:55 -0500

    Added new project: BlogSite

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -4,6 +4,7 @@ import './Projects.css';
 const github_icon = new URL('../assets/Icons/github.png', import.meta.url).href;
 const new_tab_icon = new URL('../assets/Icons/tab.png', import.meta.url).href;
 
+const blog_site = new URL('../assets/BlogSiteJPG.JPG', import.meta.url).href;
 const to_do_list = new URL('../assets/to_do_list.JPG', import.meta.url).href;
 const aurotax = new URL('../assets/aurotax.JPG', import.meta.url).href;
 
@@ -14,6 +15,27 @@ export function Projects () {
             <h2 className="projects_title">Projects</h2>
 
             <section id="projects" className="projects_container">
+
+                <div className="project_container">
+                    <h3>BlogSite</h3>
+                    
+                    <div className="project_image_container">
+                        <img src={blog_site}/>
+                    </div>
+
+                    <div className="project_techs_container">
+                        <span>React</span>
+                        <span>TypeScript / JavaScript</span>
+                        <span>CSS</span>
+                    </div>
+
+                    <p>This static site is the result of Platzi's React-Router-Dom course, and me practing React context in general. The site allows you to create text blogs, and provides a pseudo auth, which was created for learning purposes.</p>
+                    
+                    <div className="project_links_container">
+                        <a href="https://github.com/santiagredo/BlogSite" target="_blank"><span><img alt="" src={github_icon}/></span></a>
+                        <a href="https://santiagredo.github.io/BlogSite/#/home" target="_blank"><span><img alt="" src={new_tab_icon}/></span></a>
+                    </div>
+                </div>
 
                 <div className="project_container">
                     <h3>To Do list</h3>
